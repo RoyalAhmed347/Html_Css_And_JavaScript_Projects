5c551d6d4a440df2442553aee988d897deeb8aae
Royal Ahmed <125104645+RoyalAhmed347@users.noreply.github.com>
2023-09-17 21:35:54 +0500

    Update app.js

--- Projects/07_Notes Taking App/js/app.js
+++ Projects/07_Notes Taking App/js/app.js
@@ -3,23 +3,68 @@ let addNoteBtn = document.getElementById('addNote');
 let main = document.getElementById('main');
 
 
-addNoteBtn.addEventListener('click', function () {
-    addNote();
-})
+const saveNotes = () =>{
+    const notes = document.querySelectorAll('.note textarea');
+
+    const data = [];
+    notes.forEach((note)=>{
+        data.push(note.value);
+    })
+    if (data.length == 0) {
+        localStorage.removeItem("notes")
+    } else {
+    localStorage.setItem("notes", JSON.stringify(data))
+        
+    }
+}
 
 
-let addNote = () => {
+let addNote = (noteData = "") => {
     let note = document.createElement('div');
     note.classList.add('note');
     note.innerHTML = `<div class="tool">
+    <p class="name" contenteditable="true">Your Notes</p>
+    <div>
     <i class="save fas fa-save"></i>
     <i class="trash fas fa-trash"></i>
     </div>
-    <textarea name="" id="" ></textarea>`;
+    </div>
+    <textarea placeholder="Enter something">${noteData}</textarea>`;
 
     main.appendChild(note);
 
-    note.querySelector('.trash').addEventListener('click',function(){
+    note.querySelector('.trash').addEventListener('click',()=>{
         note.remove();
+        saveNotes();
+    })
+
+    note.querySelector('.save').addEventListener('click',()=>{
+        saveNotes();
+    })
+    note.querySelector('textarea').addEventListener('focusout',()=>{
+    saveNotes();
     })
 }
+
+addNoteBtn.addEventListener('click', () => {
+    addNote();
+    saveNotes();
+})
+
+
+const notesLoad = () => {
+    const lsnotes = JSON.parse(localStorage.getItem("notes"))
+
+   
+
+    if (lsnotes == null) {
+        addNote();
+        
+    } else{
+        lsnotes.forEach((note)=>{
+            addNote(note);
+        })
+    }
+}
+
+notesLoad();
